03bbdbeded13ec437fb002ea5f61f6fce11901a9
Edward Castillo <castilloedwarda@gmail.com>
2019-10-22 14:00:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,15 @@
 
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+end
+
+def link 
+  phrase = "It's Dangerous To Go Alone!"
+  puts phrase
 end
